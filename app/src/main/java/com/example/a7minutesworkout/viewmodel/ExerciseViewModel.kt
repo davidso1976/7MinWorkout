c1801b0ca418a04d83201141b1e06dbae51a798f
davidso1976 <dsanchez197694@gmail.com>
2025-07-22 21:57:31 -0700

    Updating dependencies, adding dagger for Dependency injection

---
+++ app/src/main/java/com/example/a7minutesworkout/viewmodel/ExerciseViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.a7minutesworkout.viewmodel
+
+import javax.inject.Inject
+
+class ExerciseViewModel @Inject constructor()  {
+    val timeInterval = 1
+    val restingTime = 4
+    val exerciseTimerDuration = 10
+}
